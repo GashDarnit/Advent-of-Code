e954ed9a027171f19d7263022f8266dc4da96d36
GashDarnit <edisontanh@gmail.com>
2023-12-06 13:57:56 +0800

    Updated Solution.java Template

--- .Template/Solution.java
+++ .Template/Solution.java
@@ -1,5 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
 class Solution {
     public static void main(String[] args) {
-        
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String[] inputs = lines.toArray(new String[0]);
     }   
 }
