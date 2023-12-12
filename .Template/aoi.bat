c9fad136119fb8e7228d034356c81165f88acb5f
GashDarnit <edisontanh@gmail.com>
2023-12-12 18:33:11 +0800

    Added input grabber

---
+++ .Template/aoi.bat
@@ -0,0 +1,12 @@
+@echo off
+
+if "%2"=="" (
+  echo Usage: %0 ^<year^>
+  exit /b 1
+)
+
+set day=%2
+set year=%1
+set cookie=<Session Cookie>
+
+curl -H "Cookie: session=%cookie%" "https://adventofcode.com/%year%/day/%day%/input" > "input.txt"
