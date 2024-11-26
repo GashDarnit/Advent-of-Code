c79061fe1ec06a4b534f02a4e6f208fd5b63ef6e
GashDarnit <128578806+GashDarnit@users.noreply.github.com>
2024-11-26 14:50:46 +0800

    Updated aoi.bat to create relevant directories and move the necessary files into it

--- .Template/aoi.bat
+++ .Template/aoi.bat
@@ -7,6 +7,21 @@ if "%2"=="" (
 
 set day=%2
 set year=%1
+
 set cookie=<Session Cookie>
 
+if not exist "%cd%\..\%year%" (
+    mkdir "%cd%\..\%year%"
+)
+
+if not exist "%cd%\..\%year%\Day %day%" (
+    mkdir "%cd%\..\%year%\Day %day%"
+)
+
 curl -H "Cookie: session=%cookie%" "https://adventofcode.com/%year%/day/%day%/input" > "input.txt"
+
+move "input.txt" "%cd%\..\%year%\Day %day%\input.txt"
+
+:: Copy Solution.java and run.bat into the day folder
+copy "Solution.java" "%cd%\..\%year%\Day %day%\Solution.java"
+copy "run.bat" "%cd%\..\%year%\Day %day%\run.bat"
